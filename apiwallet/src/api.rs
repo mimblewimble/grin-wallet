89ccd71661bfa9d5e9823cc383b57261ccc44447
Andrew Dirksen <andrew@dirksen.com>
2019-03-05 13:20:56 -0800

    doctest ownerapi, update easyjsonrpc version

--- apiwallet/src/api.rs
+++ apiwallet/src/api.rs
@@ -55,90 +55,18 @@ const USER_MESSAGE_MAX_LEN: usize = 256;
 /// Public definition used to generate jsonrpc api.
 pub trait OwnerAPI {
 	/**
-	Returns a list of accounts stored in the wallet (i.e. mappings between
-	user-specified labels and BIP32 derivation paths.
-
-	# Returns
-	* Result Containing:
-	* A Vector of [`AcctPathMapping`](../types/struct.AcctPathMapping.html) data
-	* or [`libwallet::Error`](../struct.Error.html) if an error is encountered.
-
-	# Remarks
-
-	* A wallet should always have the path with the label 'default' path defined,
-	with path m/0/0
-	* This method does not need to use the wallet seed or keychain.
-
-	# Example
-	Set up as in [`new`](struct.APIOwner.html#method.new) method above.
-
-	``` ignore
-	# extern crate grin_wallet_config as config;
-	# extern crate grin_refwallet as wallet;
-	# extern crate grin_keychain as keychain;
-	# extern crate grin_util as util;
-	# use std::sync::Arc;
-	# use util::Mutex;
-	# use keychain::ExtKeychain;
-	# use wallet::libwallet::api::APIOwner;
-	# use wallet::{LMDBBackend, HTTPNodeClient, WalletBackend};
-	# use config::WalletConfig;
-	# let mut wallet_config = WalletConfig::default();
-	# wallet_config.data_file_dir = "test_output/doc/wallet1".to_owned();
-	# let node_client = HTTPNodeClient::new(&wallet_config.check_node_api_http_addr, None);
-	# let mut wallet:Arc<Mutex<WalletBackend<HTTPNodeClient, ExtKeychain>>> =
-	# Arc::new(Mutex::new(
-	# 	LMDBBackend::new(wallet_config.clone(), "", node_client).unwrap()
-	# ));
-	let api_owner = APIOwner::new(wallet.clone());
-
-	let result = api_owner.accounts();
-
-	if let Ok(accts) = result {
-		//...
-	}
-	```
+	Networked version of [APIOwner::accounts](struct.APIOwner.html#method.accounts).
 
 	# Json rpc example
 
 	```
-	# fn json_rpc_owner_doctest(request: serde_json::Value, expected_response: serde_json::Value) {
-	#   use easy_jsonrpc::JSONRPCServer;
-	# 	use grin_apiwallet::api::{APIOwner, OwnerAPI};
-	# 	use grin_keychain::ExtKeychain;
-	# 	use grin_refwallet::{HTTPNodeClient, LMDBBackend, WalletBackend};
-	# 	use grin_util::Mutex;
-	# 	use grin_wallet_config::WalletConfig;
-	# 	use serde_json;
-	# 	use std::sync::Arc;
-	#
-	# 	let mut wallet_config = WalletConfig::default();
-	# 	wallet_config.data_file_dir = "test_output/doc/wallet1".to_owned();
-	# 	let node_client = HTTPNodeClient::new(&wallet_config.check_node_api_http_addr, None);
-	# 	let wallet: Arc<Mutex<WalletBackend<HTTPNodeClient, ExtKeychain>>> = Arc::new(Mutex::new(
-	# 		LMDBBackend::new(wallet_config.clone(), "", node_client).unwrap(),
-	# 	));
-	# 	let api_owner = APIOwner::new(wallet);
-	# 	let owner_api = &api_owner as &dyn OwnerAPI;
-	#
-	# 	assert_eq!(
-	# 		owner_api
-	# 			.handle_parsed(serde_json::from_value(request).unwrap())
-	# 			.unwrap(),
-	# 		serde_json::from_value(expected_response).unwrap(),
-	# 	);
-	# }
-	#
-	# use serde_json::json;
-	# json_rpc_owner_doctest(json!(
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
 	{
 		"jsonrpc": "2.0",
 		"method": "accounts",
 		"params": [],
 		"id": 1
-	}
-	# ),
-	# json!(
+	},
 	{
 		"jsonrpc": "2.0",
 		"result": {
@@ -151,24 +79,85 @@ pub trait OwnerAPI {
 		},
 		"id": 1
 	}
-	# ));
+	# );
 	```
 	 */
 	fn accounts(&self) -> Result<Vec<AcctPathMapping>, ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::create_account_path](struct.APIOwner.html#method.create_account_path).
+
+	# Json rpc example
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "create_account_path",
+		"params": ["account1"],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Ok": "0200000001000000000000000000000000"
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn create_account_path(&self, label: &String) -> Result<Identifier, ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::set_active_account](struct.APIOwner.html#method.set_active_account).
+
+	# Json rpc example
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "set_active_account",
+		"params": ["default"],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Ok": null
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn set_active_account(&self, label: &String) -> Result<(), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::retrieve_outputs](struct.APIOwner.html#method.retrieve_outputs).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "retrieve_outputs",
+		"params": [false, false, null],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn retrieve_outputs(
 		&self,
 		include_spent: bool,
@@ -176,9 +165,30 @@ pub trait OwnerAPI {
 		tx_id: Option<u32>,
 	) -> Result<(bool, Vec<(OutputData, pedersen::Commitment)>), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::retrieve_txs](struct.APIOwner.html#method.retrieve_txs).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "retrieve_txs",
+		"params": [false, null, null],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn retrieve_txs(
 		&self,
 		refresh_from_node: bool,
@@ -186,18 +196,60 @@ pub trait OwnerAPI {
 		tx_slate_id: Option<Uuid>,
 	) -> Result<(bool, Vec<TxLogEntry>), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::retrieve_summary_info](struct.APIOwner.html#method.retrieve_summary_info).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "retrieve_summary_info",
+		"params": [false, 1],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn retrieve_summary_info(
 		&self,
 		refresh_from_node: bool,
 		minimum_confirmations: u64,
 	) -> Result<(bool, WalletInfo), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::estimate_initiate_tx](struct.APIOwner.html#method.estimate_initiate_tx).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "estimate_initiate_tx",
+		"params": [null, 0, 0, 10, 0, false],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn estimate_initiate_tx(
 		&self,
 		src_acct_name: Option<String>,
@@ -208,44 +260,269 @@ pub trait OwnerAPI {
 		selection_strategy_is_use_all: bool,
 	) -> Result<(/* total */ u64, /* fee */ u64), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::finalize_tx](struct.APIOwner.html#method.finalize_tx).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "finalize_tx",
+		"params": [{
+			"amount": 0,
+			"fee": 0,
+			"height": 0,
+			"id": "414bad48-3386-4fa7-8483-72384c886ba3",
+			"lock_height": 0,
+			"num_participants": 2,
+			"participant_data": [],
+			"tx": {
+				"body": {
+					"inputs": [],
+					"kernels": [],
+					"outputs": []
+				},
+				"offset": "0000000000000000000000000000000000000000000000000000000000000000"
+			},
+			"version": 1
+		}],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn finalize_tx(&self, slate: Slate) -> Result<Slate, ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::cancel_tx](struct.APIOwner.html#method.cancel_tx).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "cancel_tx",
+		"params": [null, null],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn cancel_tx(&self, tx_id: Option<u32>, tx_slate_id: Option<Uuid>) -> Result<(), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::get_stored_tx](struct.APIOwner.html#method.get_stored_tx).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "get_stored_tx",
+		"params": [
+			{
+				"amount_credited": 0,
+				"amount_debited": 0,
+				"confirmation_ts": null,
+				"confirmed": false,
+				"creation_ts": "2019-03-05T20:49:59.444095Z",
+				"fee": null,
+				"id": 10,
+				"messages": null,
+				"num_inputs": 0,
+				"num_outputs": 0,
+				"parent_key_id": "0000000000000000000000000000000000",
+				"stored_tx": null,
+				"tx_slate_id": null,
+				"tx_type": "TxReceived"
+			}
+		],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Ok": null
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn get_stored_tx(&self, entry: &TxLogEntry) -> Result<Option<Transaction>, ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::post_tx](struct.APIOwner.html#method.post_tx).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "post_tx",
+		"params": [
+			{
+				"body": {
+					"inputs": [],
+					"kernels": [],
+					"outputs": []
+				},
+				"offset": "0000000000000000000000000000000000000000000000000000000000000000"
+			},
+			false
+		],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"ClientCallback": "Posting transaction to node: Request error: Cannot make request: an error occurred trying to connect: Connection refused (os error 61)"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn post_tx(&self, tx: &Transaction, fluff: bool) -> Result<(), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::verify_slate_messages](struct.APIOwner.html#method.verify_slate_messages).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "verify_slate_messages",
+		"params": [{
+			"amount": 0,
+			"fee": 0,
+			"height": 0,
+			"id": "414bad48-3386-4fa7-8483-72384c886ba3",
+			"lock_height": 0,
+			"num_participants": 2,
+			"participant_data": [],
+			"tx": {
+				"body": {
+					"inputs": [],
+					"kernels": [],
+					"outputs": []
+				},
+				"offset": "0000000000000000000000000000000000000000000000000000000000000000"
+			},
+			"version": 1
+		}],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			 "Ok": null
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn verify_slate_messages(&self, slate: &Slate) -> Result<(), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::restore](struct.APIOwner.html#method.restore).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "restore",
+		"params": [],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn restore(&self) -> Result<(), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::check_repair](struct.APIOwner.html#method.check_repair).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "check_repair",
+		"params": [],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn check_repair(&self) -> Result<(), ErrorKind>;
 
-	/// ``` ignore
-	/// panic!("doctest not written")
-	/// ```
+	/**
+	Networked version of [APIOwner::node_height](struct.APIOwner.html#method.node_height).
+
+
+	```
+	# grin_apiwallet::doctest_helper_json_rpc_owner_assert_response!(
+	{
+		"jsonrpc": "2.0",
+		"method": "node_height",
+		"params": [],
+		"id": 1
+	},
+	{
+		"jsonrpc": "2.0",
+		"result": {
+			"Err": {
+				"CallbackImpl": "Error opening wallet"
+			}
+		},
+		"id": 1
+	}
+	# );
+	```
+	 */
 	fn node_height(&self) -> Result<(u64, bool), ErrorKind>;
 }
 
@@ -1265,4 +1542,60 @@ where
 		w.close()?;
 		Ok(())
 	}
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! doctest_helper_json_rpc_owner_assert_response {
+	($request:tt, $expected_response:tt) => {
+		// create temporary wallet, run jsonrpc request on owner api of wallet, delete wallet, return
+		// json response.
+		// In order to prevent leaking tempdirs, This function should not panic.
+		fn rpc_owner_result(
+			request: serde_json::Value,
+		) -> Result<Option<serde_json::Value>, String> {
+			use easy_jsonrpc::Handler;
+			use grin_apiwallet::api::{APIOwner, OwnerAPI};
+			use grin_keychain::ExtKeychain;
+			use grin_refwallet::{HTTPNodeClient, LMDBBackend, WalletBackend};
+			use grin_util::Mutex;
+			use grin_wallet_config::WalletConfig;
+			use serde_json;
+			use std::sync::Arc;
+			use tempfile::tempdir;
+
+			let dir = tempdir().map_err(|e| format!("{:#?}", e))?;
+				{
+				let mut wallet_config = WalletConfig::default();
+				wallet_config.data_file_dir = dir
+					.path()
+					.to_str()
+					.ok_or("Failed to convert tmpdir path to string.".to_owned())?
+					.to_owned();
+				let node_client =
+					HTTPNodeClient::new(&wallet_config.check_node_api_http_addr, None);
+				let wallet: Arc<Mutex<WalletBackend<HTTPNodeClient, ExtKeychain>>> =
+					Arc::new(Mutex::new(
+						LMDBBackend::new(wallet_config.clone(), "", node_client)
+							.map_err(|e| format!("{:#?}", e))?,
+					));
+				let api_owner = APIOwner::new(wallet);
+				let owner_api = &api_owner as &dyn OwnerAPI;
+				Ok(owner_api.handle_request(request))
+				}
+			}
+
+		let response = rpc_owner_result(serde_json::json!($request))
+			.unwrap()
+			.unwrap();
+		let expected_response = serde_json::json!($expected_response);
+
+		if response != expected_response {
+			panic!(
+				"(left != right) \nleft: {}\nright: {}",
+				serde_json::to_string_pretty(&response).unwrap(),
+				serde_json::to_string_pretty(&expected_response).unwrap()
+				);
+			}
+	};
 }
