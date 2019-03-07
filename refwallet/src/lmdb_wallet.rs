e2998e8d542cdb4046d9541905dc5d9661fb0689
yeastplume <yeastplume@protonmail.com>
2019-03-07 09:19:47 +0000

    update database name

--- refwallet/src/lmdb_wallet.rs
+++ refwallet/src/lmdb_wallet.rs
@@ -111,7 +111,7 @@ impl<C, K> LMDBBackend<C, K> {
 		fs::create_dir_all(&stored_tx_path)
 			.expect("Couldn't create wallet backend tx storage directory!");
 
-		let store = store::Store::new(db_path.to_str().unwrap(), None, None)?;
+		let store = store::Store::new(db_path.to_str().unwrap(), None, Some(DB_DIR), None)?;
 
 		// Make sure default wallet derivation path always exists
 		// as well as path (so it can be retrieved by batches to know where to store
