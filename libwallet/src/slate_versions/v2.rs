a8f50c52990214af4dde5ee8e440c1e643d5fdf8
Yeastplume <yeastplume@protonmail.com>
2019-12-03 16:40:59 +0000

    re-insert v2 slate

--- libwallet/src/slate_versions/v2.rs
+++ libwallet/src/slate_versions/v2.rs
@@ -12,10 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Contains V3 of the slate (grin-wallet 3.0.0)
-//! Changes from V2:
-//! * Addition of payment_proof (PaymentInfo struct)
-//! * Addition of a u64 ttl_cutoff_height field
+//! Contains V2 of the slate (grin-wallet 1.1.0)
+//! Changes from V1:
+//! * ParticipantData struct fields serialized as hex strings instead of arrays:
+//!    * public_blind_excess
+//!    * public_nonce
+//!    * part_sig
+//!    * message_sig
+//! * Transaction fields serialized as hex strings instead of arrays:
+//!    * offset
+//! * Input field serialized as hex strings instead of arrays:
+//!    commit
+//! * Output fields serialized as hex strings instead of arrays:
+//!    commit
+//!    proof
+//! * TxKernel fields serialized as hex strings instead of arrays:
+//!    commit
+//!    signature
+//! * version field removed
+//! * VersionCompatInfo struct created with fields and added to beginning of struct
+//!    version: u16
+//!    orig_version: u16,
+//!    block_header_version: u16,
 
 use crate::grin_core::core::transaction::OutputFeatures;
 use crate::grin_core::libtx::secp_ser;
@@ -25,22 +43,19 @@ use crate::grin_util::secp::key::PublicKey;
 use crate::grin_util::secp::pedersen::{Commitment, RangeProof};
 use crate::grin_util::secp::Signature;
 use crate::slate::CompatKernelFeatures;
-use crate::slate_versions::ser as dalek_ser;
-use ed25519_dalek::PublicKey as DalekPublicKey;
-use ed25519_dalek::Signature as DalekSignature;
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct SlateV3 {
+pub struct SlateV2 {
 	/// Versioning info
-	pub version_info: VersionCompatInfoV3,
+	pub version_info: VersionCompatInfoV2,
 	/// The number of participants intended to take part in this transaction
 	pub num_participants: usize,
 	/// Unique transaction ID, selected by sender
 	pub id: Uuid,
 	/// The core transaction data:
 	/// inputs, outputs, kernels, kernel offset
-	pub tx: TransactionV3,
+	pub tx: TransactionV2,
 	/// base amount (excluding fee)
 	#[serde(with = "secp_ser::string_or_u64")]
 	pub amount: u64,
@@ -53,26 +68,14 @@ pub struct SlateV3 {
 	/// Lock height
 	#[serde(with = "secp_ser::string_or_u64")]
 	pub lock_height: u64,
-	/// TTL, the block height at which wallets
-	/// should refuse to process the transaction and unlock all
-	/// associated outputs
-	#[serde(with = "secp_ser::opt_string_or_u64")]
-	pub ttl_cutoff_height: Option<u64>,
 	/// Participant data, each participant in the transaction will
 	/// insert their public data here. For now, 0 is sender and 1
 	/// is receiver, though this will change for multi-party
-	pub participant_data: Vec<ParticipantDataV3>,
-	/// Payment Proof
-	#[serde(default = "default_payment_none")]
-	pub payment_proof: Option<PaymentInfoV3>,
-}
-
-fn default_payment_none() -> Option<PaymentInfoV3> {
-	None
+	pub participant_data: Vec<ParticipantDataV2>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct VersionCompatInfoV3 {
+pub struct VersionCompatInfoV2 {
 	/// The current version of the slate format
 	pub version: u16,
 	/// Original version this slate was converted from
@@ -82,7 +85,7 @@ pub struct VersionCompatInfoV3 {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct ParticipantDataV3 {
+pub struct ParticipantDataV2 {
 	/// Id of participant in the transaction. (For now, 0=sender, 1=rec)
 	#[serde(with = "secp_ser::string_or_u64")]
 	pub id: u64,
@@ -102,19 +105,9 @@ pub struct ParticipantDataV3 {
 	pub message_sig: Option<Signature>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct PaymentInfoV3 {
-	#[serde(with = "dalek_ser::dalek_pubkey_serde")]
-	pub sender_address: DalekPublicKey,
-	#[serde(with = "dalek_ser::dalek_pubkey_serde")]
-	pub receiver_address: DalekPublicKey,
-	#[serde(with = "dalek_ser::option_dalek_sig_serde")]
-	pub receiver_signature: Option<DalekSignature>,
-}
-
 /// A transaction
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct TransactionV3 {
+pub struct TransactionV2 {
 	/// The kernel "offset" k2
 	/// excess is k1G after splitting the key k = k1 + k2
 	#[serde(
@@ -123,21 +116,21 @@ pub struct TransactionV3 {
 	)]
 	pub offset: BlindingFactor,
 	/// The transaction body - inputs/outputs/kernels
-	pub body: TransactionBodyV3,
+	pub body: TransactionBodyV2,
 }
 
 /// TransactionBody is a common abstraction for transaction and block
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct TransactionBodyV3 {
+pub struct TransactionBodyV2 {
 	/// List of inputs spent by the transaction.
-	pub inputs: Vec<InputV3>,
+	pub inputs: Vec<InputV2>,
 	/// List of outputs the transaction produces.
-	pub outputs: Vec<OutputV3>,
+	pub outputs: Vec<OutputV2>,
 	/// List of kernels that make up this transaction (usually a single kernel).
-	pub kernels: Vec<TxKernelV3>,
+	pub kernels: Vec<TxKernelV2>,
 }
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct InputV3 {
+pub struct InputV2 {
 	/// The features of the output being spent.
 	/// We will check maturity for coinbase output.
 	pub features: OutputFeatures,
@@ -150,7 +143,7 @@ pub struct InputV3 {
 }
 
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
-pub struct OutputV3 {
+pub struct OutputV2 {
 	/// Options for an output's structure or use
 	pub features: OutputFeatures,
 	/// The homomorphic commitment representing the output amount
@@ -168,7 +161,7 @@ pub struct OutputV3 {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct TxKernelV3 {
+pub struct TxKernelV2 {
 	/// Options for a kernel's structure or use
 	pub features: CompatKernelFeatures,
 	/// Fee originally included in the transaction this proof is for.
@@ -194,178 +187,11 @@ pub struct TxKernelV3 {
 
 /// A mining node requests new coinbase via the foreign api every time a new candidate block is built.
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct CoinbaseV3 {
+pub struct CoinbaseV2 {
 	/// Output
-	pub output: OutputV3,
+	pub output: OutputV2,
 	/// Kernel
-	pub kernel: TxKernelV3,
+	pub kernel: TxKernelV2,
 	/// Key Id
 	pub key_id: Option<Identifier>,
-}
-
-// V2 to V3 For Slate
-impl From<SlateV2> for SlateV3 {
-	fn from(slate: SlateV2) -> SlateV3 {
-		let SlateV2 {
-			num_participants,
-			id,
-			tx,
-			amount,
-			fee,
-			height,
-			lock_height,
-			ttl_cutoff_height,
-			participant_data,
-			version_info,
-			payment_proof,
-		} = slate;
-		let participant_data = map_vec!(participant_data, |data| ParticipantData::from(data));
-		let version_info = VersionCompatInfo::from(&version_info);
-		let payment_proof = match payment_proof {
-			Some(p) => Some(PaymentInfo::from(&p)),
-			None => None,
-		};
-		let tx = Transaction::from(tx);
-		Slate {
-			num_participants,
-			id,
-			tx,
-			amount,
-			fee,
-			height,
-			lock_height,
-			ttl_cutoff_height,
-			participant_data,
-			version_info,
-			payment_proof,
-		}
-	}
-}
-
-impl From<&ParticipantDataV3> for ParticipantData {
-	fn from(data: &ParticipantDataV3) -> ParticipantData {
-		let ParticipantDataV3 {
-			id,
-			public_blind_excess,
-			public_nonce,
-			part_sig,
-			message,
-			message_sig,
-		} = data;
-		let id = *id;
-		let public_blind_excess = *public_blind_excess;
-		let public_nonce = *public_nonce;
-		let part_sig = *part_sig;
-		let message: Option<String> = message.as_ref().map(|t| String::from(&**t));
-		let message_sig = *message_sig;
-		ParticipantData {
-			id,
-			public_blind_excess,
-			public_nonce,
-			part_sig,
-			message,
-			message_sig,
-		}
-	}
-}
-
-impl From<&VersionCompatInfoV3> for VersionCompatInfo {
-	fn from(data: &VersionCompatInfoV3) -> VersionCompatInfo {
-		let VersionCompatInfoV3 {
-			version,
-			orig_version,
-			block_header_version,
-		} = data;
-		let version = *version;
-		let orig_version = *orig_version;
-		let block_header_version = *block_header_version;
-		VersionCompatInfo {
-			version,
-			orig_version,
-			block_header_version,
-		}
-	}
-}
-
-impl From<&PaymentInfoV3> for PaymentInfo {
-	fn from(data: &PaymentInfoV3) -> PaymentInfo {
-		let PaymentInfoV3 {
-			sender_address,
-			receiver_address,
-			receiver_signature,
-		} = data;
-		let sender_address = *sender_address;
-		let receiver_address = *receiver_address;
-		let receiver_signature = *receiver_signature;
-		PaymentInfo {
-			sender_address,
-			receiver_address,
-			receiver_signature,
-		}
-	}
-}
-
-impl From<TransactionV3> for Transaction {
-	fn from(tx: TransactionV3) -> Transaction {
-		let TransactionV3 { offset, body } = tx;
-		let body = TransactionBody::from(&body);
-		Transaction { offset, body }
-	}
-}
-
-impl From<&TransactionBodyV3> for TransactionBody {
-	fn from(body: &TransactionBodyV3) -> TransactionBody {
-		let TransactionBodyV3 {
-			inputs,
-			outputs,
-			kernels,
-		} = body;
-
-		let inputs = map_vec!(inputs, |inp| Input::from(inp));
-		let outputs = map_vec!(outputs, |out| Output::from(out));
-		let kernels = map_vec!(kernels, |kern| TxKernel::from(kern));
-		TransactionBody {
-			inputs,
-			outputs,
-			kernels,
-		}
-	}
-}
-
-impl From<&InputV3> for Input {
-	fn from(input: &InputV3) -> Input {
-		let InputV3 { features, commit } = *input;
-		Input { features, commit }
-	}
-}
-
-impl From<&OutputV3> for Output {
-	fn from(output: &OutputV3) -> Output {
-		let OutputV3 {
-			features,
-			commit,
-			proof,
-		} = *output;
-		Output {
-			features,
-			commit,
-			proof,
-		}
-	}
-}
-
-impl From<&TxKernelV3> for TxKernel {
-	fn from(kernel: &TxKernelV3) -> TxKernel {
-		let (fee, lock_height) = (kernel.fee, kernel.lock_height);
-		let features = match kernel.features {
-			CompatKernelFeatures::Plain => KernelFeatures::Plain { fee },
-			CompatKernelFeatures::Coinbase => KernelFeatures::Coinbase,
-			CompatKernelFeatures::HeightLocked => KernelFeatures::HeightLocked { fee, lock_height },
-		};
-		TxKernel {
-			features,
-			excess: kernel.excess,
-			excess_sig: kernel.excess_sig,
-		}
-	}
 }
