fffe6b98dde9da03cecf917e43cd768d4571386a
ardocrat <ardocrat@gri.mw>
2026-07-22 15:52:25 +0300

    config: remove unused error type

--- config/src/types.rs
+++ config/src/types.rs
@@ -141,9 +141,6 @@ pub enum ConfigError {
 
 	/// Path doesn't exist
 	PathNotFoundError(String),
-
-	/// Other errors
-	Other(String),
 }
 
 impl fmt::Display for ConfigError {
@@ -164,9 +161,6 @@ impl fmt::Display for ConfigError {
 				write!(f, "Error serializing configuration: {}", message)
 			}
 			ConfigError::PathNotFoundError(ref message) => write!(f, "Path not found: {}", message),
-			ConfigError::Other(ref message) => {
-				write!(f, "{}", message)
-			}
 		}
 	}
 }
