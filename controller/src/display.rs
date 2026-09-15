b495575048a5521a4423ebd31b8e3612d4c55dd4
ardocrat <ardocrat@gri.mw>
2026-09-15 22:43:50 +0300

    fix: some typos, imports, unwraps and formatting

--- controller/src/display.rs
+++ controller/src/display.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::core::core::amount_to_hr_string;
 use crate::core::core::FeeFields;
-use crate::core::core::{self, amount_to_hr_string};
 use crate::core::global;
 use crate::libwallet::{
 	AcctPathMapping, Error, OutputCommitMapping, OutputStatus, TxLogEntry, ViewWallet, WalletInfo,
@@ -43,7 +43,7 @@ pub fn outputs(
 	}
 	let mut t = term::stdout().unwrap();
 	let _ = t.fg(term::color::MAGENTA);
-	writeln!(t, "{}", title).unwrap();
+	writeln!(t, "{}", title)?;
 	let _ = t.reset();
 
 	let mut table = table!();
@@ -77,7 +77,7 @@ pub fn outputs(
 		};
 
 		let num_confirmations = format!("{}", m.output.num_confirmations(cur_height));
-		let value = format!("{}", core::amount_to_hr_string(m.output.value, false));
+		let value = format!("{}", amount_to_hr_string(m.output.value, false));
 		let tx = match m.output.tx_log_entry {
 			None => "".to_owned(),
 			Some(t) => t.to_string(),
@@ -117,7 +117,7 @@ pub fn outputs(
 	if !validated {
 		println!(
 			"\nWARNING: Wallet failed to verify data. \
-			 The above is from local cache and possibly invalid! \
+			 The above is from local cache and possibly invalid! \
 			 (is your `grin server` offline or broken?)"
 		);
 	}
@@ -144,7 +144,7 @@ pub fn txs(
 	}
 	let mut t = term::stdout().unwrap();
 	let _ = t.fg(term::color::MAGENTA);
-	writeln!(t, "{}", title).unwrap();
+	writeln!(t, "{}", title)?;
 	let _ = t.reset();
 
 	let mut table = table!();
@@ -192,18 +192,18 @@ pub fn txs(
 		let confirmed = format!("{}", t.confirmed);
 		let num_inputs = format!("{}", t.num_inputs);
 		let num_outputs = format!("{}", t.num_outputs);
-		let amount_debited_str = core::amount_to_hr_string(t.amount_debited, true);
-		let amount_credited_str = core::amount_to_hr_string(t.amount_credited, true);
+		let amount_debited_str = amount_to_hr_string(t.amount_debited, true);
+		let amount_credited_str = amount_to_hr_string(t.amount_credited, true);
 		let fee = match t.fee {
-			Some(f) => format!("{}", core::amount_to_hr_string(f.fee(), true)),
+			Some(f) => format!("{}", amount_to_hr_string(f.fee(), true)),
 			None => "None".to_owned(),
 		};
 		let net_diff = if t.amount_credited >= t.amount_debited {
-			core::amount_to_hr_string(t.amount_credited - t.amount_debited, true)
+			amount_to_hr_string(t.amount_credited - t.amount_debited, true)
 		} else {
 			format!(
 				"-{}",
-				core::amount_to_hr_string(t.amount_debited - t.amount_credited, true)
+				amount_to_hr_string(t.amount_debited - t.amount_credited, true)
 			)
 		};
 		let tx_data = match t.stored_tx {
@@ -289,7 +289,7 @@ pub fn txs(
 	if !validated && include_status {
 		println!(
 			"\nWARNING: Wallet failed to verify data. \
-			 The above is from local cache and possibly invalid! \
+			 The above is from local cache and possibly invalid! \
 			 (is your `grin server` offline or broken?)"
 		);
 	}
@@ -334,7 +334,7 @@ pub fn view_wallet_output(
 
 	let mut t = term::stdout().unwrap();
 	let _ = t.fg(term::color::MAGENTA);
-	writeln!(t, "{}", title).unwrap();
+	writeln!(t, "{}", title)?;
 	let _ = t.reset();
 
 	let mut table = table!();
@@ -356,7 +356,7 @@ pub fn view_wallet_output(
 		let lock_height = format!("{}", m.lock_height);
 		let is_coinbase = format!("{}", m.is_coinbase);
 		let num_confirmations = format!("{}", m.num_confirmations(cur_height));
-		let value = format!("{}", core::amount_to_hr_string(m.value, false));
+		let value = format!("{}", amount_to_hr_string(m.value, false));
 
 		if dark_background_color_scheme {
 			table.add_row(row![
@@ -408,11 +408,11 @@ pub fn info(
 		]);
 		if wallet_info.amount_reverted > 0 {
 			table.add_row(row![
-				Fr->format!("Reverted"),
+				Fr->"Reverted",
 				Fr->amount_to_hr_string(wallet_info.amount_reverted, false)
 			]);
 		}
-		// Only dispay "Immature Coinbase" if we have related outputs in the wallet.
+		// Only display "Immature Coinbase" if we have related outputs in the wallet.
 		// This row just introduces confusion if the wallet does not receive coinbase rewards.
 		if wallet_info.amount_immature > 0 {
 			table.add_row(row![
@@ -425,7 +425,7 @@ pub fn info(
 			FY->amount_to_hr_string(wallet_info.amount_awaiting_confirmation, false)
 		]);
 		table.add_row(row![
-			bFB->format!("Awaiting Finalization"),
+			bFB->"Awaiting Finalization",
 			FB->amount_to_hr_string(wallet_info.amount_awaiting_finalization, false)
 		]);
 		table.add_row(row![
@@ -447,11 +447,11 @@ pub fn info(
 		]);
 		if wallet_info.amount_reverted > 0 {
 			table.add_row(row![
-				Fr->format!("Reverted"),
+				Fr->"Reverted",
 				Fr->amount_to_hr_string(wallet_info.amount_reverted, false)
 			]);
 		}
-		// Only dispay "Immature Coinbase" if we have related outputs in the wallet.
+		// Only display "Immature Coinbase" if we have related outputs in the wallet.
 		// This row just introduces confusion if the wallet does not receive coinbase rewards.
 		if wallet_info.amount_immature > 0 {
 			table.add_row(row![
@@ -482,7 +482,7 @@ pub fn info(
 	if !validated {
 		println!(
 			"\nWARNING: Wallet failed to verify data against a live chain. \
-			 The above is from local cache and only valid up to the given height! \
+			 The above is from local cache and only valid up to the given height! \
 			 (is your `grin server` offline or broken?)"
 		);
 	}
@@ -532,7 +532,7 @@ pub fn estimate(
 
 /// Display list of wallet accounts in a pretty way
 pub fn accounts(acct_mappings: Vec<AcctPathMapping>) {
-	println!("\n____ Wallet Accounts ____\n",);
+	println!("\n____ Wallet Accounts ____\n");
 	let mut table = table!();
 
 	table.set_titles(row![
@@ -552,7 +552,7 @@ pub fn accounts(acct_mappings: Vec<AcctPathMapping>) {
 
 /// Display individual Payment Proof
 pub fn payment_proof(tx: &TxLogEntry) -> Result<(), Error> {
-	let title = format!("Payment Proof - Transaction '{}'", tx.id,);
+	let title = format!("Payment Proof - Transaction '{}'", tx.id);
 	println!();
 	if term::stdout().is_none() {
 		println!("Could not open terminal");
@@ -560,12 +560,12 @@ pub fn payment_proof(tx: &TxLogEntry) -> Result<(), Error> {
 	}
 	let mut t = term::stdout().unwrap();
 	let _ = t.fg(term::color::MAGENTA);
-	writeln!(t, "{}", title).unwrap();
+	writeln!(t, "{}", title)?;
 	let _ = t.reset();
 
 	let pp = match &tx.payment_proof {
 		None => {
-			writeln!(t, "None").unwrap();
+			writeln!(t, "None")?;
 			let _ = t.reset();
 			return Ok(());
 		}
@@ -573,7 +573,7 @@ pub fn payment_proof(tx: &TxLogEntry) -> Result<(), Error> {
 	};
 
 	let _ = t.fg(term::color::WHITE);
-	writeln!(t).unwrap();
+	writeln!(t)?;
 	let receiver_signature = match pp.receiver_signature {
 		Some(s) => {
 			let sig_bytes = s.to_bytes();
@@ -587,11 +587,11 @@ pub fn payment_proof(tx: &TxLogEntry) -> Result<(), Error> {
 		None => 0,
 	};
 	let amount = if tx.amount_credited >= tx.amount_debited {
-		core::amount_to_hr_string(tx.amount_credited - tx.amount_debited, true)
+		amount_to_hr_string(tx.amount_credited - tx.amount_debited, true)
 	} else {
 		format!(
 			"{}",
-			core::amount_to_hr_string(tx.amount_debited - tx.amount_credited - fee, true)
+			amount_to_hr_string(tx.amount_debited - tx.amount_credited - fee, true)
 		)
 	};
 
@@ -615,18 +615,16 @@ pub fn payment_proof(tx: &TxLogEntry) -> Result<(), Error> {
 		t,
 		"Receiver Address: {}",
 		OnionV3Address::from_bytes(pp.receiver_address.to_bytes())
-	)
-	.unwrap();
-	writeln!(t, "Receiver Signature: {}", receiver_signature).unwrap();
-	writeln!(t, "Amount: {}", amount).unwrap();
-	writeln!(t, "Kernel Excess: {}", kernel_excess).unwrap();
+	)?;
+	writeln!(t, "Receiver Signature: {}", receiver_signature)?;
+	writeln!(t, "Amount: {}", amount)?;
+	writeln!(t, "Kernel Excess: {}", kernel_excess)?;
 	writeln!(
 		t,
 		"Sender Address: {}",
 		OnionV3Address::from_bytes(pp.sender_address.to_bytes())
-	)
-	.unwrap();
-	writeln!(t, "Sender Signature: {}", sender_signature).unwrap();
+	)?;
+	writeln!(t, "Sender Signature: {}", sender_signature)?;
 
 	let _ = t.reset();
 
